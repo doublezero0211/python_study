c3f8fc94730037e4891a14bcc36b9f94b62e49fa
zero <doublezero0211@gmail.com>
2019-11-29 17:56:38 +0800

    update 20191129 prepare deploy to heroku

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -132,3 +132,20 @@ SILENCED_SYSTEM_CHECKS = ['fields.E300', 'fields.E307']
 BOOTSTRAP3 = {
         'include_jquery': True,
         }
+
+# Heroku的设置
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATANASES = {
+        'default': dj_database_url.config(default='postgres://localhost')        
+    }
+    # 让request.is_secure()承认X-Forward-Proto的头
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO', 'https')
+    # 支持所有的主机头(host header)
+    ALLOWED_HOSTS = ['*']
+    # 静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
