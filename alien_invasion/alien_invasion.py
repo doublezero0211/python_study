4c23a055888b2cc67783803e3b3ff4b447154d67
zero <doublezero0211@gmail.com>
2019-12-05 18:01:18 +0800

    update 20191205

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -12,10 +13,14 @@ def run_game():
     pygame.display.set_caption("Alient Invasion")
     # 创建飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个存储子弹的编组
+    bullets = Group()
+
     # 开始游戏主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
