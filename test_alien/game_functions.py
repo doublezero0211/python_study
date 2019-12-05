4434ae632711d87f7eb98c219a131e6ff3a96c5f
zero <doublezero0211@gmail.com>
2019-12-05 21:08:26 +0800

    add testing

---
+++ test_alien/game_functions.py
@@ -0,0 +1,66 @@
+import sys
+import pygame
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_top = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_bottom = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """若未达到限制，就发射子弹"""
+    # 创建一颗子弹，并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_top = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_bottom = False
+
+def check_events(ai_settings, screen, ship, bullets):
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+
+def update_screen(ai_settings, screen, ship, bullets):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时，重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    
+    ship.blitme()
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
+
+def update_bullets(bullets, screen):
+    screen_rect = screen.get_rect()
+    bullets.update()
+    # 删除消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.left >= screen_rect.right:
+            bullets.remove(bullet)
